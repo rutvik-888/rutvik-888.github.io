ae929f74da056ad779e21ff275709c2303e2c6fe
rutvik-888 <119109593+rutvik-888@users.noreply.github.com>
2023-06-27 17:59:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-$(document).ready(function() {
-  $('.title').click(function() {
+$(document).ready(function(){
+  $('.title').click(function(){
     $('.container').addClass('open');
   });
-
-  $('.close').click(function() {
+  
+  
+  $('.close').click(function(){
     $('.container').removeClass('open');
   });
 });
